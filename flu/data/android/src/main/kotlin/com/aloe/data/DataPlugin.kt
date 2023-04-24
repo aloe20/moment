e7c64e235f0ba5cfe252b2841e2517cbe082fd93
aloe <zhaloe@163.com>
2023-04-25 07:20:52 +0800

    优化代码

--- flu/data/android/src/main/kotlin/com/aloe/data/DataPlugin.kt
+++ flu/data/android/src/main/kotlin/com/aloe/data/DataPlugin.kt
@@ -17,6 +17,7 @@
 package com.aloe.data
 
 import androidx.annotation.NonNull
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -25,10 +26,10 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 /** DataPlugin */
 class DataPlugin : FlutterPlugin, MethodCallHandler {
-    // / The MethodChannel that will the communication between Flutter and native Android
-    // /
-    // / This local reference serves to register the plugin with the Flutter Engine and unregister it
-    // / when the Flutter Engine is detached from the Activity
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
